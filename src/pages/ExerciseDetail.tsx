82aab1f0039b7c05cda9097383272961fc035ce6
Ibrahimovic <ibrahimovic@MacBook-Air-de-Ibrahimovic.local>
2023-10-06 19:37:33 +0200

    fix

--- src/pages/ExerciseDetail.tsx
+++ src/pages/ExerciseDetail.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
 import {AppDispatch, RootState} from '../store'
 import {getExerciceById, getEquipment, getTarget} from '../actions/exercises/exercises.actions'
 
 import Detail from '../components/Detail';
-import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 import { Box } from '@mui/material';
 
@@ -17,8 +16,6 @@ interface ExerciseDetailProps {
 }
 
 const ExerciseDetail: React.FC = () => {
-  const [exerciseDetail, setExerciseDetail] = useState<ExerciseDetailProps | null>(null);
-  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams<{ id: string }>();
   const dispatch = useDispatch<AppDispatch>();
   const { exercises } = useSelector((state: RootState) => state);
@@ -37,7 +34,6 @@ const ExerciseDetail: React.FC = () => {
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
       <Detail exerciseDetail={exercises.exercice} />
-     {/* <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />*/}
       <SimilarExercises targetMuscleExercises={exercises.target} equipmentExercises={exercises.equipment} />
     </Box>
   );
